d8723c8426426743cfa3d007f455da4e78973c6c
Pruthvin07 <82469042+Pruthvin07@users.noreply.github.com>
2022-01-03 19:22:52 +0530

    Dice Game

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var randomnumber1 = Math.floor(Math.random()*6) + 1;
+var randomDiceImg = "dice"+randomnumber1+".png"
+var randomImageSource = "images/"+randomDiceImg;
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+var randomnumber2 = Math.floor((Math.random()*6) + 1);
+var randomImageSource2 = "images/dice"+ randomnumber2 +".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+if(randomnumber1 > randomnumber2){
+    document.querySelector("h1").innerHTML="1️⃣🏆Player 1 Wins";
+}
+else if(randomnumber1 < randomnumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins🏆2️⃣";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw🤝";
+}
